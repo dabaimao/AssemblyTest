8b9f2a7972470bc9eea2581d1f3faeab7668f357
dabaimao <shawnping@outlook.com>
2017-11-09 17:07:30 +0800

    数据已经能显示但是元素排列反向问题有待解决

--- Test103.asm
+++ Test103.asm
@@ -1,68 +1,78 @@
-assume cs:code,ds:data,ss:stack
+assume cs:code
 		data segment
-			db 32 dup(0)
+			db 12,30,2,10
+			db 28 dup(0)
+			dw 123,12666,1,8,3,38,0
 		data ends
+		
 		stack segment
 			db 32 dup(0)
 		stack ends
 		
 		code segment
 			start:
-			mov ax,32
-			mov sp,ax
-			mov ax,12666
-			mov si,0
+			mov ax,data
+			mov ds,ax
+			mov di,ds:[3]
+			mov si,4
 			call dtoc
-			mov si,0
-			mov dh,8
-			mov dl,80
-			mov cl,2
+			
+			mov si,4
+			mov dh,ds:[0]    ;字符串输出行位置  
+			mov dl,ds:[1]   ;字符串输出列位置  
+			mov cl,ds:[2]    ;显示模式（8位）  
 			call show_str
 			
-			mov ax,4c00H
+			mov ax,4c00h
 			int 21h
 			
 			dtoc:
-				dc0:
-				jcxz dc1
-				div ax		;除法溢出准备改进
-				add dx,30h
-				pop dx
-				mov cx,dx
-				loop dc0
-				
-				dc1:
-				jcxz resdd
-				mov cx,bx
-				pop bx
-				mov ds:[si],bx
-				inc si
-				loop dc1				
-			resdd:
-			ret
+				mov bx,32
+				dch:
+					mov ax,[bx]
+					mov cx,ax
+					jcxz over
+					dsn:
+						mov cx,ax
+						jcxz next
+						div di
+						add dx,30h
+						mov [si],dl
+						inc si
+						mov dx,0
+					loop dsn
+					next:
+					inc si
+					mov ax,20h
+					mov [si],ax
+					add bx,2
+				loop dch
+			over:
+				ret
 			
-			show_str:
-				mov ax,0B800H
-				mov es,ax
-				mov ax,160
-				dec dh
-				mul dh
-				mov dh,0
-				dec dl
-				add dl,dl
-				add ax,dx
-				mov di,ax
-				mov ah,cl
+			show_str:   ;子程序  
+				mov ax,0b800h     
+				mov es,ax   ;es寄存器指向显存段  
+				mov ax,160  ;行位置控制↓  
+				dec dh  
+				mul dh  
+				mov dh,0    ;行位置控制↑  
+				dec dl      ;列位置控制↓  
+				add dl,dl   ;列位置控制↑  
+				add ax,dx  
+				mov di,ax  
+				mov ah,cl   ;计算最终输出位置=行x180+列  
 				
-				display:
-				mov cx,[si]
-				jcxz reshow
-				mov al,[si]		
-				mov es:[di],ax
-				add di,2
-				inc si
-				loop display
-			reshow:
-			ret
+				
+				display:    ;输出字符串子程序  
+					mov cx,ds:[si]  
+					jcxz return  
+					mov al,ds:[si]       
+					mov es:[di],ax  
+					add di,2  
+					inc si  
+				loop display  
+			return:
+				ret        ;返回call show_str  
 		code ends
 end start
