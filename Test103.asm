a2d2c9e8c47933971a07ef37b7415aace50645f7
dabaimao <shawnping@outlook.com>
2017-11-10 11:35:57 +0800

    新的改进暂时没完成

--- Test103.asm
+++ Test103.asm
@@ -1,25 +1,26 @@
 assume cs:code
 		data segment
-			db 12,30,2,10
+			db 12,30,10100100b,10
 			db 28 dup(0)
 			dw 123,12666,1,8,3,38,0
 		data ends
-		
+
 		stack segment
 			db 32 dup(0)
 		stack ends
-		
+
 		code segment
 			start:
 			mov ax,data
 			mov ds,ax
+			mov ax,stack
+			mov ss,ax
+			mov sp,32
 			mov di,ds:[3]
-			mov si,4
+			mov si,0
 			call dtoc
-			
-			mov si,4
 			mov dh,ds:[0]    ;字符串输出行位置  
-			mov dl,ds:[1]   ;字符串输出列位置  
+			mov dl,ds:[1]   	;字符串输出列位置  
 			mov cl,ds:[2]    ;显示模式（8位）  
 			call show_str
 			
@@ -28,27 +29,26 @@ assume cs:code
 			
 			dtoc:
 				mov bx,32
-				dch:
+				chdata:
 					mov ax,[bx]
 					mov cx,ax
 					jcxz over
-					dsn:
+					divtest:
 						mov cx,ax
-						jcxz next
+						jcxz after
 						div di
 						add dx,30h
-						mov [si],dl
-						inc si
+						push dx
 						mov dx,0
-					loop dsn
-					next:
-					inc si
-					mov ax,20h
-					mov [si],ax
-					add bx,2
-				loop dch
-			over:
-				ret
+						inc si
+					loop divtest
+					after:
+						mov cx,si 
+						pop ax
+						mov [si+4],ax
+
+				loop chdata
+			over:ret
 			
 			show_str:   ;子程序  
 				mov ax,0b800h     
@@ -61,9 +61,8 @@ assume cs:code
 				add dl,dl   ;列位置控制↑  
 				add ax,dx  
 				mov di,ax  
-				mov ah,cl   ;计算最终输出位置=行x180+列  
-				
-				
+				mov ah,cl   ;计算最终输出位置=行x180+列  			
+
 				display:    ;输出字符串子程序  
 					mov cx,ds:[si]  
 					jcxz return  
@@ -73,6 +72,6 @@ assume cs:code
 					inc si  
 				loop display  
 			return:
-				ret        ;返回call show_str  
+			ret        ;返回call show_str
 		code ends
 end start
